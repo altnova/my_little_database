4c10aaee660bbcbc613e646c40737ff43ad1c801
altnova <36820255+altnova@users.noreply.github.com>
2018-05-06 20:49:19 +0300

    get_num() rewritten

--- menu.c
+++ menu.c
@@ -6,71 +6,100 @@
 #define I int
 #define C char
 
+I pow_(I num, I exp)
+{
+	I i, n = num;
+	if (!exp)
+		R 1;
+	for (i = 0; i < exp; i++) 
+		num *= n;
+	R num;
+}
+
+/*	returns number from input or error (-1)	*/
 I get_num()
 {
-	I num = 0, i;
-	C c = '1';
-	for (i = 0; c != '\n'; i++) {
-		c = getchar();
-		if (c == '\n')
-			break;
-		else 
-			c -= '0';
+	I num = 0, i, i_, j;
+	C buf[15], c = '1';
 
-		if (c < 0 || c > 9)	 		// if c is not a digit
-			R -1;
-		num += c * pow(10, i);
+	for (i = 0; i < 14; i++)
+		buf[i] = 0;
+
+	for (i = 0; i < 15 && c != '\n'; i++) {
+		scanf("%c", &c);
+		if (c == '\n') {
+			buf[i] = '\0';
+			break;
+		}
+		buf[i] = c;
+	}
+
+	if (c != '\n') {
+		while (c != '\n')
+			scanf("%c", &c);
+		R -1;
+	}
+
+	for (i = 14; buf[i] <= 32 || buf[i] == 127; i--);
+
+
+	for (i_ = i; i >= 0; i--) {
+		c = buf[i];
+		if (c < '0' || c > '9') 						// if c is not a digit
+			R -1;	
+		num += (c - '0') * pow_(10, i_ - i);
 	}
 	R num;
 }
 
+
 /*	main menu	*/
-void scr_main_0();
+void scr_main_0(); 
 
 /*	exit program	*/
-void scr_exit_0();
+void scr_exit_0() {}
 
 /*	search record 	*/
-void scr_search_1();
+void scr_search_1() {}
 
 /*	add record 	*/
-void scr_addrec_2();
+void scr_addrec_2() {}
 
 /*	delete record 	*/
-void scr_deleterec_3();
+void scr_deleterec_3() {}
 
 /*	edit record 	*/
-void scr_editrec_4();
+void scr_editrec_4() {}
 
 /*	display record 	*/
-void scr_displayrec_5();
+void scr_displayrec_5() {}
 
 /*	display all records */
-void scr_displayall_6();
+void scr_displayall_6() {}
 
 /* 	display database status */
-void scr_dbstat_7();
+void scr_dbstat_7() {}
 
 /* 	vacuum database 	*/
-void scr_dbvacuum_8();
+void scr_dbvacuum_8() {}
 
 
 
 void scr_main_0()
 {
-	// I command;
+	I command;
 	O("1. search record\n2. add record\n3. delete record\n");
-	O("4.edit record\n5. display record\n6. display all records\n");
-	O("7. database status\n8. vaccum database\n\n 0. exit program\n");
-	// command = get_num();
+	O("4. edit record\n5. display record\n6. display all records\n");
+	O("7. database status\n8. vaccum database\n\n0. exit program\n");
+	command = get_num();
 
-	switch (get_num()) {
+	switch (command) {
 		case 0:
 		scr_exit_0(); 			// 	exit program
 		break;
 
 		case 1:
-		scr_search_1():			//	search records
+		scr_search_1();			//	search records
 		break;
 
 		case 2:
