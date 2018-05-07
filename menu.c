f44b9f7060b157aa2628b5aacb1c138dd956f4fe
kelas <me@kel.as>
2018-05-07 15:52:57 +0200

    [+] more idiomatic

--- menu.c
+++ menu.c
@@ -1,155 +1,86 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#define R return
-#define O printf
-#define I int
-#define C char
-#define UJ unsigned long
+#include "books.h"
 
-/*	returns number from input or error (-1)	*/
-void get_num(UJ *num)
-{
-	C c = '1';
+#define VER "1.0.0"
+#define MI(i,label) O("\t%d. %s\n", i, label);
+#define NL() O("\n");
 
-	for (*num = 0; c != '\n'; ) {
-		scanf("%c", &c);
-		if (c == '\n') 
-			break;
+V scr_main_0();
+V scr_search_1();
+V scr_search_1_1(I);
 
-		if (c < '0' || c > '9') {
-			while (c != '\n')
-				scanf("%c", &c);
-			*num = -1;
-			R;
-		}
-		*num *= 10;
-		*num += c - '0';
-	}
-	// printf("%d!\n", num);
+void banner() {
+	O("\tAmazon Kindle Database v%s\n", VER);
+	O("\t_____________________________\n");
 }
 
-/*	main menu	*/
-I scr_main_0(); 
-/*	exit program	*/
-// void scr_exit_0() {}
-/*	search record 	*/
-I scr_search_1();
-void scr_search_1_1();
-/*	add record 	*/
-// void scr_addrec_2() {}
-/*	delete record 	*/
-// void scr_deleterec_3() {}
-/*	edit record 	*/
-// void scr_editrec_4() {}
-/*	display record 	*/
-// void scr_displayrec_5() {}
-/*	display all records */
-// void scr_displayall_6() {}
-/* 	display database status */
-// void scr_dbstat_7() {}
-/* 	vacuum database 	*/
-// void scr_dbvacuum_8() {}
-
-
-I scr_search_1(UJ *command)
-{
-	O("1. by year\n2. by title\n3. by author\n");
-	O("4. by subject\n\n0. main menu\n\n");
-	O("select menu item: ");
-
-	get_num(command);
-	switch (*command) {
-		case 0:
-		R 0;
-		break;
-
-	/*	case 1:
-		scr_search_1_1(fld_year);
-		break;
-
-		case 2:
-		scr_search_1_1(fld_title);
-		break;
-
-		case 3:
-		scr_search_1_1(fld_author);
-		break;
-
-		case 4:
-		scr_search_1_1(fld_subject);
-		break;*/
-
-		default:
-		O("\nERROR: unknown command\n\n");
-		R 1;
-		break;
-	}
-	R 0;
+I input_num(int *number, S prompt) {
+    C line[4096];
+    while(O("%s: ", prompt) > 0 && fgets(line, sizeof(line), stdin) != 0) {
+        if (sscanf(line, "%d", number) == 1)
+            return 0;
+        printf("ERR: invalid format\n");
+    }
+    return EOF;
 }
 
+V scr_search_1() {
+	I choice;
 
-
-I scr_main_0(UJ *command)
-{
-	O("1. search record\n2. add record\n3. delete record\n");
-	O("4. edit record\n5. display record\n6. display all records\n");
-	O("7. database status\n8. vaccum database\n\n0. exit program\n\n");
-	O("select menu item: ");
-	get_num(command);
-
-	switch (*command) {
-		case 0:
-		R 0;	 			// 	exit program
-		break;
-
-		case 1:
-		while (scr_search_1(command));
-		R 1;
-	
-		break;
-
-	/*	case 2:
-		scr_addrec_2();			// 	add record
-		break;
-
-		case 3:
-		scr_deleterec_3();		//	delete record
-		break;
-
-		case 4:
-		scr_editrec_4();		//	edit record
-		break;
-
-		case 5:
-		scr_displayrec_5();		//	display record
-		break;
-
-		case 6:
-		scr_displayall_6();		//	display all records
-		break;
-
-		case 7:
-		scr_dbstat_7();			//	database status
-		break;
-
-		case 8:
-		scr_dbvacuum_8();		//	vacuum database 
-		break;
-*/
-		default:
-		O("\nERROR: unknown command\n\n");
-		R 1;
+	NL();
+	O("\tSearch record\n");
+	O("\t-------------\n");
+	NL();
+	MI(1, "By Year")
+	MI(2, "By Title")
+	MI(3, "By Author")
+	MI(4, "By Subject")
+	NL()
+	MI(0, "Main Menu")
+	NL();
+	input_num(&choice, "Select field");
+	SW(choice){
+		CS(0, R)
+		CS(1, scr_search_1_1(fld_year))
+		CS(2, scr_search_1_1(fld_title))
+		CS(3, scr_search_1_1(fld_author))
+		CS(4, scr_search_1_1(fld_subject))
+		CD:O("\nERR: unknown command\n\n");
 	}
-
-R 0;
 }
 
+V scr_main_0() {
+	I choice;
 
-I main()
-{
-	UJ command;
-	while (scr_main_0(&command));
+	NL();
+	banner();
+	NL();
+	MI(1, "Search record")
+	MI(2, "Add record")
+	MI(3, "Delete record")
+	MI(4, "Edit record")
+	MI(5, "Display record")
+	MI(6, "Display all records")
+	MI(7, "Database status")
+	NL()
+	MI(0, "Exit program")
+	NL()
+
+	input_num(&choice, "Select menu item");
+	SW(choice){
+		CS(0, O("Goodbye.\n"); exit(0))
+		CS(1, scr_search_1())
+		CD:O("\nERR: unknown command\n\n");
+	}
+}
+
+V scr_search_1_1(I fld) {
+}
+
+I main() {
+	while(1)scr_main_0();
 	R 0;
 }
 
