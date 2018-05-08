16d8e3d801e8b2dd7596dbcf7c24cdca2a4724ab
kelas <me@kel.as>
2018-05-08 21:00:39 +0200

    [~] trc messages

--- src/rec.c
+++ src/rec.c
@@ -29,14 +29,14 @@ UJ rec_get_db_pos(ID rec_id) {
 	UJ idx_pos = rec_get_idx_pos(rec_id);
 	BAIL_IF(idx_pos, NONE);
 	Pair *e = idx_get_entry(idx_pos);
-	T(TRACE, "rec_get_db_pos: { rec_id=%lu, idx_pos=%lu, db_pos=%lu }\n", e->rec_id, idx_pos, e->pos);
+	T(TRACE, "{ rec_id=%lu, idx_pos=%lu, db_pos=%lu }", e->rec_id, idx_pos, e->pos);
 	R e->pos;}
 
 //! load record into dest by rec_id
-UJ rec_get(Rec dest, UJ rec_id) {
+UJ rec_get(Rec dest, ID rec_id) {
 	LOG("rec_get");
 	UJ db_pos = rec_get_db_pos(rec_id);
-	T(TRACE, "rec_get: { rec_id=%lu, db_pos=%lu }\n", rec_id, db_pos);
+	T(TRACE, "{ rec_id=%lu, db_pos=%lu }", rec_id, db_pos);
 	BAIL_IF(db_pos, NONE);
 	FILE *db = fopen(db_file, "r");
 	zseek(db, db_pos*SZ_REC, SEEK_SET);
@@ -45,11 +45,11 @@ UJ rec_get(Rec dest, UJ rec_id) {
 	R db_pos;}
 
 //! delete record from db and index
-UJ rec_delete(UJ rec_id) {
+UJ rec_delete(ID rec_id) {
 	LOG("rec_delete");
 	UJ db_pos = rec_get_db_pos(rec_id);
 	BAIL_IF(db_pos, NONE);
-	T(DEBUG, "rec_delete: { rec_id=%lu, db_pos=%lu }\n", rec_id, db_pos);	
+	T(DEBUG, "deleting { rec_id=%lu, db_pos=%lu }", rec_id, db_pos);	
 	
 	FILE *db = fopen(db_file, "r+");
 
@@ -58,18 +58,18 @@ UJ rec_delete(UJ rec_id) {
 	J offset = SZ_REC*last_pos;
 	zseek(db, offset, SEEK_SET);
 	fread(b, SZ_REC, 1, db);	//< read last record
-	T(DEBUG, "rec_delete: loaded tail record { rec_id=%lu, pos=%lu, offset=%ld }\n", b->rec_id, last_pos, offset);
+	T(DEBUG, "loaded tail record { rec_id=%lu, pos=%lu, offset=%ld }", b->rec_id, last_pos, offset);
 	zseek(db, db_pos*SZ_REC, SEEK_SET);
 	fwrite(b, SZ_REC, 1, db); //< overwrite deleted record
 	idx_update_pos(b->rec_id, db_pos);
 	idx_update_pos(rec_id, NONE);
-	T(DEBUG, "rec_delete: overwritten record { rec_id=%lu, db_pos=%lu }\n", rec_id, db_pos);
+	T(DEBUG, "overwritten record { rec_id=%lu, db_pos=%lu }", rec_id, db_pos);
 	free(b);
 
 	UJ new_size = idx_shift(db_pos);
 	ftrunc(db, SZ_REC*new_size);
-	T(TRACE, "rec_delete: db file truncated\n");
-	
+	T(TRACE, "db file truncated");
+
 	fclose(db);
 	R db_pos;}
 
@@ -81,7 +81,7 @@ UJ rec_create(Rec r) {
 	UJ db_pos = fsize(db)/SZ_REC;
 	fwrite(r, SZ_REC, 1, db);
 	fclose(db);
-	T(DEBUG, "rec_create: { rec_id=%lu, pos=%lu }\n", r->rec_id, db_pos);
+	T(DEBUG, "rec_id=%lu, pos=%lu", r->rec_id, db_pos);
 	idx_add(r->rec_id, db_pos);
 	R db_pos;}
 
@@ -93,9 +93,9 @@ UJ rec_update(Rec r) {
 	FILE *db = fopen(db_file, "r+");
 	UJ offset = SZ_REC*db_pos;
 	zseek(db, offset, SEEK_SET);
-	fwrite(r, SZ_REC, 1, db); //< overwrite old data
+	fwrite(r, SZ_REC, 1, db); //< overwrite old version
 	fclose(db);
-	T(DEBUG, "rec_update: { rec_id=%lu }\n", r->rec_id);
+	T(DEBUG, "rec_id=%lu", r->rec_id);
 	R db_pos;}
 
 //! update field
@@ -104,7 +104,7 @@ V rec_set(Rec r, I fld, V*val) {
 	I offset = rec_field_offsets[fld];
 	I len = fld<3?SZ(H)-1:MIN(csv_max_field_widths[fld],strlen(val));
 	memcpy(((V*)r)+offset, val, len+1);
-	T(TRACE, "rec_set: { rec_id=%lu, fld=%d, len=%d }\n", r->rec_id, fld, len);}
+	T(TRACE, "rec_id=%lu, fld=%d, len=%d", r->rec_id, fld, len);}
 
 
 //:~
