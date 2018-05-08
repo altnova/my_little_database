83763e5d54c02bc5e813a85641b597fba8bc5387
kelas <me@kel.as>
2018-05-08 20:35:36 +0200

    [~]

--- src/trc.c
+++ src/trc.c
@@ -6,15 +6,15 @@
 #include "trc.h"
 
 //! trace
-I T(I lvl, S fn, S file, I line, S format, ...) {
-	va_list args;
-	va_start(args, format);
+I T(I lvl, S fn, S file, I line, S fmt, ...) {
 	if(lvl <= LOGLEVEL) {
-		C buf[strlen(fn)+strlen(format)+strlen(file)+30];
-		snprintf(buf, sizeof buf, "%s:%d\t[%s] %s\n", file, line, fn, format);
+		va_list args;
+		va_start(args, fmt);
+		C buf[strlen(fn)+strlen(fmt)+strlen(file)+30];
+		snprintf(buf, SZ(buf), "%s:%d\t[%s] %s\n", file, line, fn, format);
 		vprintf(buf, args);
+		va_end(args);
 	}
-	va_end(args);
 	R 1; //< err
 }
 
