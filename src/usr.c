ac24c9512349f42d8700d1f98300e4dc15d28617
kelas <me@kel.as>
2018-05-08 18:31:33 +0200

    [~]

--- src/usr.c
+++ src/usr.c
@@ -2,20 +2,15 @@
 #include "___.h"
 
 I input_number(I *num, S prompt, S errmsg){
-    char line[4096];
-
+    C line[4096];
     while(O("%s: ", prompt) > 0 && fgets(line, SZ(line), stdin) != 0)
-    {
-        if(sscanf(line, "%d", num) == 1)
-            R 0;
-        O("%s\n", errmsg);
-    }
+        if(sscanf(line, "%d", num) == 1)R 0;else O("%s\n", errmsg);
     R EOF;
 }
 
 I main(V){
 	I n;
-	input_number(&n);
+    input_number(&n);
 	O("%d\n", n);
 } 
 
