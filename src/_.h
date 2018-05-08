07bf7c9355cd6a6677256a77f09a8a5b3fca6d58
kelas <me@kel.as>
2018-05-08 15:39:33 +0200

    [~]

--- src/_.h
+++ src/_.h
@@ -51,7 +51,7 @@ typedef struct books {
 #define MAX(x,y) ((y)>(x)?(y):(x))
 
 // string manipulation
-#define scnt(x)   (UJ)strlen((S)(x))			//!< string length \param str
+#define scnt(x)   (UJ)strlen((S)(x))		//!< string length \param str
 #define scmp(x,y) strcmp((S)(x),(S)(y))     //!< compare two strings \param x str \param y str
 #define scpy(x,y) (S)strcpy((S)(x),(S)(y))  //!< copy x into y \param x srcstr \param y deststr
 #define scpy_s(x,y,n) (S)memcpy((S)(x),(S)(y),MIN(scnt((S)y),n))  //!< secure copy max n bytes from y into x \param x dststr \param y srcstr \param n limit
