10ebbe8d5cca144e5756e220a6440175ecbc4ce5
kelas <me@kel.as>
2018-05-10 21:10:38 +0200

    [~] doc

--- src/csv.h
+++ src/csv.h
@@ -1,5 +1,14 @@
-//!\file csv.h \brief csv parse api
+//! \file csv.h \brief csv parser api
 
-extern UJ csv_load(S fname)
+//! load csv file
+//! \return #records loaded, NIL on error
+extern UJ csv_load(S fname);
+
+//! initialize csv parser
+//! \return 0 on success, NIL on error
 extern UJ csv_init(S db_fname);
+
+//! release resources
 extern V  csv_close();
+
+//:~
