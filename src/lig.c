b93f48ca0bbbb0103d830128525e59c873fa59ec
kelas <me@kel.as>
2018-05-11 08:52:14 +0200

    [+]

---
+++ src/lig.c
@@ -0,0 +1,50 @@
+//! test fira code retina ligatures
+V*;
+char*;
+*asdf;
+
+V* ligtest() {
+	asdf(*(*asdf),a);
+	**asdf;
+	a,&a;
+	a,**a;
+	
+	asdf(*x);
+	LOG("asdf");
+	char *aasdf;
+	I* asdf;
+	I *asdf;
+	int* asdf;
+	(int*)asdf;
+	(V*)(asdf);
+	FILE *fp;
+	FILE*fp;
+	char *c;
+	2*2;
+	*asdf;
+	asfd*asdf;
+	(asdf + *asdf);
+	*(asdf+asdf);
+	(*asdf);
+	asdf[*asdf];
+	*(*(*(*asdf));
+	&asdf;
+	&(asdf)
+	[&asdf];
+	asdf&asdf;
+
+	*asdf;
+
+	// x->field field accessor
+	/* hinted comment tags */ 
+	// compressed double slash
+	if(x==y||x!=y||x&&y||x>=y||x<=y){} //< comparison
+	x++;x--;!!x;!x;~x;	//< good idea to highlight these guys
+	x^y;
+	x||y;
+	x%y;
+	x&y;
+	x?1:2;
+	x<<=1;
+	y>>=2;
+}
---
+++ sub/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# mac
+cp C-kelas.sublime-syntax ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
+cp C-kelas.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
+cp monokai-kelas.tmTheme ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
+
+# lin
