da3adb50edd6a481e7681da1fe3a9f5a9e4873e4
kelas <me@kel.as>
2018-05-11 21:20:35 +0200

    [+] detect platform

--- sub/install.sh
+++ sub/install.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 
-# mac
-#cp C-kelas.sublime-syntax ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
-#cp C-kelas.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
-#cp monokai-kelas.tmTheme ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=lin;;
+    Darwin*)    machine=mac;;
+    CYGWIN*)    machine=cyg;;
+    MINGW*)     machine=mgw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
 
-# lin
-cp C-kelas.sublime-syntax ~/.config/sublime-text-3/Packages/User/
-cp C-kelas.sublime-settings ~/.config/sublime-text-3/Packages/User/
-cp monokai-kelas.tmTheme ~/.config/sublime-text-3/Packages/User/
+if [ $machine == 'lin' ]; then
+	st3_path=~/.config/sublime-text-3/Packages/User/
+elif [ $machine == 'mac' ]; then
+	st3_path=~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
+else
+	echo "$machine unsupported"; exit
+fi
+
+echo "sublime => $st3_path"
+#diff C-kelas.sublime-syntax "$st3_path/C-kelas.sublime-syntax"
+#diff C-kelas.sublime-settings "$st3_path/C-kelas.sublime-settings"
+#diff monokai-kelas.tmTheme "$st3_path/monokai-kelas.tmTheme"
+
+cp C-kelas.sublime-syntax "$st3_path"
+cp C-kelas.sublime-settings "$st3_path"
+echo "c-kelas => installed"
+
+cp monokai-kelas.tmTheme "$st3_path"
+echo "monokai => installed"
