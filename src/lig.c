0bf0eece0aa9edf30f4092f0c23ff277b0c7c828
kelas <me@kel.as>
2018-05-11 11:00:36 +0200

    [+]

--- src/lig.c
+++ src/lig.c
@@ -3,49 +3,46 @@ V*;
 char*;
 *asdf;
 
-V* ligtest() {
-	asdf(*(*asdf),a);
+S syntext() {
+	fn(*(*asdf),a);
 	**asdf;
 	a,&a;
 	a,**a;
-	
+	(a)a;
 	asdf(*x);
 	LOG("asdf");
-	char *aasdf;
-	I* asdf;
-	I *asdf;
-	int* asdf;
-	(int*)asdf;
+
+	I* asdf; I *asdf;	
+	char *asdf; char *c;
+	int* asdf; (int*)asdf;
+	FILE *fp; FILE*fp;
 	(V*)(asdf);
-	FILE *fp;
-	FILE*fp;
-	char *c;
-	2*2;
+	
 	*asdf;
 	asfd*asdf;
 	(asdf + *asdf);
 	*(asdf+asdf);
 	(*asdf);
-	asdf[*asdf];
+	&asdf[*asdf];
 	*(*(*(*asdf)));
+	&(&(&(&asdf)));
 	&asdf;
-	&(asdf);
-	[&asdf];
+	&(asdf+43);
+	x[&abc];
 	asdf&asdf;
+	2*2;
+}
 
-	*asdf;
-
+V* ligtest() {
 	/* hinted comment tags */ 
 	// compressed double slash
 
 	x->asdf; // accessors!
 
-	if(x==y||x!=y||x&&y||x>=y||x<=y){} //< comparison
-	x++; x--; !!x; !x; ~x;	//< good idea to highlight these guys
-	x^y;
-	x%y;
-	x&y;
-	x?1:2;
-	x<<=1;
-	y>>=2;
+	if(x==y||x!=y||x&&y||x>=y||x<=y){
+		x++; x--; !!x; !x; ~x;
+		x^y; x%y; x&y;
+		x<<=1; y>>=2;
+		x?1:2;
+	}
 }
