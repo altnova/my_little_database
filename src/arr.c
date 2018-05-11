b14cb25cbfd7b7e19c583eb2c90c40431285056b
kelas <me@kel.as>
2018-05-11 09:11:02 +0200

    [+] syn

--- src/arr.c
+++ src/arr.c
@@ -8,8 +8,8 @@
 
 Arr arr_init_(sz n, sz t) {
 	LOG("arr_init");
-	Arr a = (Arr)malloc(SZ_HDR + n*t);
-	chk(a,0)
+	Arr a = (Arr)malloc(SZ_HDR + n * t);
+	chk(a,0);
 	a->used = 0;
 	a->size = n;
 	a->el_size = t;
@@ -17,30 +17,35 @@ Arr arr_init_(sz n, sz t) {
 
 V* arr_at_(Arr a, UJ i){
 	P(i>a->used, NULL);
-	R(V*)(a->data + i*a->el_size);}
+	R(V*)(a->data + i * a->el_size);}
 
 V* arr_last_(Arr a){
-	R(V*)(a->data + a->el_size*(a->used-1));}
+	R(V*)(a->data + a->el_size * (a->used-1));}
 
 V arr_free(Arr a){
-	if(a)free(a),a=NULL;}
+	if(a)free(a);
+	a=NULL;
+}
 
 sz arr_sz(Arr a){
-	R a->used;}
+	R a->used;
+}
 
 Z C arr_full(Arr a){
-	R a->used == a->size;}
+	R a->used==a->size;
+}
 
 //! \return 1 if fail, 0 ok
-I arr_add_(Arr* aptr, V *el){
+I arr_add_(Arr* aptr, V* el){
 	LOG("arr_add"); Arr a;
-	if(arr_full(a=*aptr)) {
+	if(arr_full(a = *aptr)) {
 		a->size *= 2, T(TRACE,"grew to %lu", arr_sz(a));
-		*aptr = a = (Arr)realloc(a, SZ_HDR + a->el_size*a->size);
-		chk(a,1)
+		*aptr = a = (Arr)realloc(a, SZ_HDR + a->el_size * a->size);
+		chk(a,1);
 	}
-	memcpy((V*)(a->data + a->el_size*a->used++), el, a->el_size);
-	R 0;}
+	memcpy((V*)(a->data + a->el_size * a->used++), el, a->el_size);
+	R0;
+}
 
 //! test
 typedef J TT; //< type
@@ -48,12 +53,13 @@ ZI arr_test(){
 	LOG("arr_test");
 	UJ t = 100; //< test iterations
 	Arr a = arr_init(5,TT);	//< initially 5 elements
-	X(!a,T(FATAL,"arr_init failed"),1)
+	X(!a,T(FATAL, "arr_init failed"),1)
 	DO(t,arr_add(a,i)) //< will grow as necessary
 	DO(t,O("%ld %ld|", i,*arr_at(a,i,TT)))
 	O("\ntotal: %lu\n", arr_sz(a)); //< print number of elements
 	arr_free(a); //< never forget
-	R 0;}
+	R0;
+}
 
 #ifdef RUN_TESTS
 I main(){R arr_test();}
