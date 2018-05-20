eca6881c7c69a3cb7cd4e9bacb1b7b58e20128de
kelas <me@kel.as>
2018-05-20 23:25:25 +0200

    [~]

--- src/str.h
+++ src/str.h
@@ -1,13 +1,13 @@
 //! \file str.h \brief string utilities
 
-//! wrap \param s string to lines of \param n length
+//! calculate wrapping of \param s to lines of \param n length
 //! \param x exec should use \c line_start,line_len
 /*! \c pts chunks to split current word into */\
 /*! \c lead leading spaces in line */\
 /*! \c tail remaining length of long word (tok_len>=n) */\
 /*! \c room available space on the current line */\
 #define str_wrap(s,n,x) {I lead, line_len=0, line_start=0, line_end=0; UJ text_len=scnt(s), tail, room;\
-	stok(s,text_len," \t\n", 0,\
+	stok(s,text_len," \t\n",0,\
 		I pts = 1+tok_len/n;if(pts>1){tail=tok_len;room=n-(tok_pos-line_start);tok_len=room;if(room)pts++;}\
 		for(I p=0;p<pts;++p){\
 			/*if(pts>1)O("DO rnd=%d tok_pos=%lu tok_len=%lu line_len=%lu tail=%lu room=%lu %.*s\n", p, tok_pos, tok_len , line_len, tail, room, tok_len, s+tok_pos);*/\
