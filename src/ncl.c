31dd17cfc957f74f945bd112b2cb241b80692930
kelas <me@kel.as>
2018-05-30 21:15:11 +0200

    [+] added rpc handlers

--- src/ncl.c
+++ src/ncl.c
@@ -25,6 +25,7 @@ ZI ncl_on_connect(I d) {
 
 ZI ncl_on_stdin(I c, UI l, S str) {
 	LOG("stdin_callback");
+	str[l-1]=0;//terminate
 	cli_dispatch_cmd(str);
 	cln_set_prompt(cli_get_prompt());
 	R0;}
@@ -38,17 +39,23 @@ I ncl_cmd_show(S arg) { // show
 
 UJ ncl_cmd_edit_send(Rec r) {
 	LOG("ncl_cmd_edit_send");
-	msg_send(c, rpc_UPD_req(1, SZ_REC, r));
-	R0;}
+	msg_send(c, rpc_UPD_req(1, r));
+	R NIL-1;}//< defer ok
+
 I ncl_cmd_edit(S arg) { // edit
 	LOG("ncl_cmd_edit");
 	cli_enter_edit_mode(CLI_PROMPT_UPDATE, "update", ncl_cmd_edit_send);
 	ncl_cmd_show(arg);
 	R0;}
 
+UJ ncl_cmd_add_send(Rec r) {
+	LOG("ncl_cmd_add_send");
+	msg_send(c, rpc_ADD_req(1, r));
+	R NIL-1;}//< defer ok
+
 I ncl_cmd_add(S arg) { // add
 	LOG("ncl_cmd_add");
-	T(TEST,"nyi");
+	cli_cmd_rec_add(arg); // native fn is good enough
 R0;}
 
 I ncl_cmd_del(S arg) { // del
@@ -58,16 +65,25 @@ I ncl_cmd_del(S arg) { // del
 	msg_send(c, rpc_DEL_req(rec_id));
 	R0;}
 
+I ncl_cmd_list(S arg) { // list
+	LOG("ncl_cmd_list");
+	cli_recalc_pager(arg);
+	cli_print_page_head();
+	msg_send(c, rpc_LST_req(
+		cli_get_current_page_id()-1,
+		cli_get_current_page_size()));
+	R0;}
+
+I ncl_cmd_search(S arg) { // debug
+	LOG("ncl_cmd_search");
+	T(TEST,"nyi");
+
 I ncl_cmd_import(S arg) { // import
 	LOG("ncl_cmd_import");
 	T(TEST,"nyi");
 R0;}
 I ncl_cmd_export(S arg) { // export
 	LOG("ncl_cmd_export");
-	T(TEST,"nyi");
-R0;}
-I ncl_cmd_list(S arg) { // list
-	LOG("ncl_cmd_list");
 	T(TEST,"nyi");
 R0;}
 I ncl_cmd_sort(S arg) { // sort
@@ -78,9 +94,6 @@ I ncl_cmd_debug(S arg) { // debug
 	LOG("ncl_cmd_debug");
 	T(TEST,"nyi");
 R0;}
-I ncl_cmd_search(S arg) { // debug
-	LOG("ncl_cmd_search");
-	T(TEST,"nyi");
 R0;}
 
 ZI ncl_on_msg(I d, MSG_HDR *h, pMSG *m) {
@@ -88,12 +101,12 @@ ZI ncl_on_msg(I d, MSG_HDR *h, pMSG *m) {
 	SW(h->type){
 		CS(SAY_res,;
 			pSAY_res *m_say = (pSAY_res*)m;
-			T(TEST, "rcvd SAY(%d): %.*s", m_say->data_len, m_say->data_len, m_say->msg);
+			T(TEST, "rcvd SAY(%d): %.*s", m_say->cnt, m_say->cnt, m_say->msg);
 		)
 		CS(HEY_res,;
 			pHEY_res *m_hey = (pHEY_res*)m;
 			cli_set_db_info((DB_INFO)m_hey->db_info);
-			cli_init();
+			cli_init(); //< will only initialize once
 		)
 		CS(GET_res,;
 			pGET_res *m_get = (pGET_res*)m;
@@ -102,11 +115,15 @@ ZI ncl_on_msg(I d, MSG_HDR *h, pMSG *m) {
 		)
 		CS(UPD_res,;
 			pUPD_res *m_upd = (pUPD_res*)m;
-			cli_print_edit_res(m_upd->cnt, 0);
+			cli_print_edit_res(m_upd->rec_id,0);
 		)
 		CS(DEL_res,;
 			pDEL_res *m_del = (pDEL_res*)m;
 			cli_print_del_res(m_del->rec_id,0);
+		)
+		CS(ADD_res,;
+			pADD_res *m_add = (pADD_res*)m;
+			cli_print_edit_res(m_add->rec_id,0);
 		)
 
 		CD: msg_send_err(d, ERR_UNKNOWN_MSG_TYPE, "unknown message type");
@@ -126,7 +143,7 @@ I main() {
 	cli_set_cmd_handler('^', ncl_cmd_sort); // sort
 	cli_set_cmd_handler('~', ncl_cmd_debug); // debug
 	cli_set_cmd_handler('?', ncl_cmd_search); // search
-	cli_set_rec_handlers(NULL, NULL, rec_set);
+	cli_set_rec_handlers(ncl_cmd_add_send, NULL, rec_set);
 	cln_init();
 
 	msg_set_callback(ncl_on_msg);
--- src/nsr.c
+++ src/nsr.c
@@ -9,6 +9,11 @@
 ZV nyi(I d) {
 	msg_send_err(d, ERR_NOT_YET_IMPLEMENTED, "not yet implemented");}
 
+Z UJ nsr_rec_each(Rec r, V*conn, UJ i) {
+	I d = *conn;
+	
+}
+
 ZI nsr_on_msg(I d, MSG_HDR *h, pMSG *m) {
 	LOG("nsr_on_msg");
 	C b[100];UJ res;ID rec_id;
@@ -16,14 +21,11 @@ ZI nsr_on_msg(I d, MSG_HDR *h, pMSG *m) {
 	SW(h->type) {
 		CS(HEY_req,;
 			pHEY_req* m_hey = (pHEY_req*)m;
-			I len = sprintf(b, "nice to meet you, %.5s", (S)m_hey->username);
+			I len = snprintf(b, 100, "nice to meet you, %s", (S)m_hey->username);
 			msg_send(d, rpc_SAY_res(len, (S)b));
-			FTI_INFO i = mem_info();
-			UJ info[3];
-			info[0] = i->total_records;
-			info[1] = i->total_tokens;
-			info[2] = i->total_mem;
-			msg_send(d, rpc_HEY_res(3*SZ(UJ), (UJ*)&info));
+			UPDATE_METRICS:
+			;DB_INFO i = mem_db_info();
+			msg_send(d, rpc_HEY_res(1, i));
 		)
     	CS(GET_req,
     		;pGET_req* m_get = (pGET_req*)m;
@@ -31,7 +33,7 @@ ZI nsr_on_msg(I d, MSG_HDR *h, pMSG *m) {
     		Rec r = (Rec)malloc(SZ_REC);chk(r,1);
     		res = rec_get(r, rec_id);
     		X(res==NIL,msg_send_err(d,ERR_NO_SUCH_RECORD,"no such record"),-1);
-    		msg_send(d, rpc_GET_res(SZ_REC, r));
+    		msg_send(d, rpc_GET_res(1, r));
     		free(r);
     	)
     	CS(DEL_req,
@@ -40,11 +42,30 @@ ZI nsr_on_msg(I d, MSG_HDR *h, pMSG *m) {
     		res = rec_delete(rec_id);
     		X(res==NIL,msg_send_err(d,ERR_NO_SUCH_RECORD,"no such record"),-1);
     		msg_send(d, rpc_DEL_res(rec_id));
+    		goto UPDATE_METRICS;
     	)
-    	CS(UPD_req, ;pUPD_req* m_upd = (pUPD_req*)m; nyi(d);)
-    	CS(ADD_req, ;pADD_req* m_add = (pADD_req*)m; nyi(d);)
+    	CS(UPD_req,
+    		;pUPD_req* m_upd = (pUPD_req*)m;
+    		X(m_upd->cnt!=1,msg_send_err(d,ERR_NOT_SUPPORTED,"updating multiple records is not implemented"),-1);
+    		res = rec_update(m_upd->records);
+    		X(res==NIL,msg_send_err(d,ERR_CMD_FAILED,"update failed"),-1);
+    		msg_send(d, rpc_UPD_res(m_upd->cnt));
+    	)
+    	CS(ADD_req,
+    		;pADD_req* m_add = (pADD_req*)m;
+    		X(m_add->cnt!=1,msg_send_err(d,ERR_NOT_SUPPORTED,"adding multiple records is not implemented"),-1);
+    		res = rec_create(m_add->records);
+    		X(res==NIL,msg_send_err(d,ERR_CMD_FAILED,"create failed"),-1);
+    		msg_send(d, rpc_ADD_res(m_add->records->rec_id));
+    		goto UPDATE_METRICS;
+    	)
+    	CS(LST_req,
+    		;pLST_req* m_lst = (pLST_req*)m;
+    		UI p = m_lst->page_num, pp = m_lst->per_page;
+			UJ res = idx_page(cli_list_rec_each, &d, p, pp);
+    	)
+
     	CS(FND_req, ;pFND_req* m_fnd = (pFND_req*)m; nyi(d);)
-    	CS(LST_req, ;pLST_req* m_lst = (pLST_req*)m; nyi(d);)
     	CS(SRT_req, ;pSRT_req* m_srt = (pSRT_req*)m; nyi(d);)
     	CS(BYE_req, ;pBYE_req* m_bye = (pBYE_req*)m; nyi(d);)
     	CS(SAY_req, ;pSAY_req* m_say = (pSAY_req*)m; nyi(d);)
@@ -58,12 +79,7 @@ ZI nsr_on_msg(I d, MSG_HDR *h, pMSG *m) {
 
 I nsr_stdin(I d, UI l, S str) {
 	LOG("nsr_stdin");
-	if(l&&!mcmp(str,"s",1)) {
-		MSG m = rpc_HEY_req(5,"server");
-		snd(d, m, msg_size(m));
-		T(TEST, "sent HEY %d bytes", msg_size(m));
-		msg_hdr_dump(&m->hdr);
-		free(m);}
+	str[l-1]=0;//terminate
 	write(d, PROMPT, scnt(PROMPT));
 	R0;}
 
